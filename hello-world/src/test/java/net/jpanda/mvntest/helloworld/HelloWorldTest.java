232fb171b9679cb7eab57e3c5459cfbe44b8afb0
yxy <yxy@jpanda.net>
2018-11-28 21:39:56 +0800

    add test code for class HelloWorld

---
+++ hello-world/src/test/java/net/jpanda/mvntest/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package net.jpanda.mvntest.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest{
+	
+	@Test
+	public void testSayHello(){
+		HelloWorld helloWorld=new HelloWorld();
+		String result=helloWorld.sayHello();
+		assertEquals("Hello Maven",result);
+	}
+}
